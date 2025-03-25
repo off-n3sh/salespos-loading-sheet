78f05ded83fd6819c27f7e81d31dc7ccffce2390
root <root@offsec.local>
2025-03-25 05:15:53 -0400

    minor import issue

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, Response, session, jsonify
+from flask import Flask, render_template, request, redirect, url_for, Response, session, jsonify, make_response
 import firebase_admin
 from firebase_admin import credentials, firestore, auth
 from datetime import datetime, timedelta
